9ee93644c2fa734e932abe12e5da4c2b5b6de7c2
Fabian Cabana <fakekian@gmail.com>
2022-11-20 18:55:58 -0500

    menus finalizados y agregado carrito de compras

--- main.js
+++ main.js
@@ -5,17 +5,28 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu'); 
 const menuIcon = document.querySelector('.menuIcon'); 
 
-const shoppingCart = document.querySelector('.product-detal'); 
+
+const shoppingCart = document.querySelector('.navbar-shopping-cart'); 
+const productAside = document.querySelector('.product-detail'); 
 
 
 
 function toggleDesktopMenu () {
     desktopMenu.classList.toggle('inactive')
+    productAside.classList.add('inactive');
 };
 
 function toggleMobileMenu () {
     mobileMenu.classList.toggle('inactive')
+    productAside.classList.add('inactive');
+};
+
+function toggleProductAside () {
+    productAside.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');  
+    desktopMenu.classList.add('inactive'); 
 };
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
 menuIcon.addEventListener('click',toggleMobileMenu);
+shoppingCart.addEventListener('click',toggleProductAside);
